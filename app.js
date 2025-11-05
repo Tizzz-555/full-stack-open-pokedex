aa49a65697c054683922dc00a10642e81463d4a6
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2025-11-05 14:50:27 +0900

    11.12.1

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`) // eslint-disable-line no-console
+  this_causes_error
+  // eslint-disable-next-line no-console
+  console.log(`server started on port ${PORT}`)
 })
 
 app.get('/version', (req, res) => {

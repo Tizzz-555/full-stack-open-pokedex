4e8444dc5c4f933eb9b99add9987bb833e50fee5
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2025-11-05 16:47:07 +0900

    11.12.4

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-undef
-  this_causes_error
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
 
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  '
+  res.send('ok')
 })

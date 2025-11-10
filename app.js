980f63f9df4d35b43ba6138335ebbdcb7ff2559c
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2025-11-10 17:42:00 +0900

    11.19.4

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
-
 // Simulate production error for health check testing
 app.use((req, res, next) => {
   if (process.env.NODE_ENV === 'production') {
@@ -13,6 +11,8 @@ app.use((req, res, next) => {
   }
   next()
 })
+
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
